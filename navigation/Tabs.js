2e3c74815765a21b815dc817d8efbb8ff04c0189
Liam Sutton <liamsutton1997@outlook.com>
2021-09-27 17:34:33 +0100

    implemented basic bottom tab navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,1 +1,24 @@
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {Icon} from 'react-native-elements'
+import HomeScreen from '../screens/HomeScreen'
+import NewBookScreen from '../screens/NewBookScreen'
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen}
+            />
+            <Tab.Screen 
+                name="NewBook" 
+                component={NewBookScreen} 
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs;
