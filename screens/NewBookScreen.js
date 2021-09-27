efe9c23eda0b32f84e8d4188beeda12f3764fd7c
Liam Sutton <liamsutton1997@outlook.com>
2021-09-27 16:58:00 +0100

    initial commit

---
+++ screens/NewBookScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const NewBookScreen = () => {
+    return(
+        <View>
+            <Text>Hello from the New Book Screen!</Text>
+        </View>
+    )
+}
+
+export default NewBookScreen
