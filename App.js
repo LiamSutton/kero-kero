d067fbe02599c6fb142977b71c50c97b8609f8e3
Liam Sutton <liamsutton1997@outlook.com>
2021-09-27 19:29:35 +0100

    added basic navigation bar

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './navigation/Tabs'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const App = () => {
   return(
+    <SafeAreaProvider>
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
+    </SafeAreaProvider>
   )
 }
 
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import {Icon} from 'react-native-elements'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import HomeScreen from '../screens/HomeScreen'
 import NewBookScreen from '../screens/NewBookScreen'
+import SearchScreen from '../screens/SearchScreen'
 
 const Tab = createBottomTabNavigator()
 
@@ -12,10 +13,53 @@ const Tabs = () => {
             <Tab.Screen 
                 name="Home" 
                 component={HomeScreen}
+                
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({focused}) => {
+                        return <Ionicons 
+                                    name={'home'} 
+                                    size={25} 
+                                    color={
+                                        focused ? 'black' : 'gray'
+                                    } 
+                                />;
+                    }
+                }}
             />
+
+            <Tab.Screen 
+                name="SearchScreen" 
+                component={SearchScreen} 
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({focused}) => {
+                        return <Ionicons 
+                                    name={'search'} 
+                                    size={25} 
+                                    color={
+                                        focused ? 'black' : 'gray'
+                                    } 
+                                />;
+                    }
+                }}
+            />
+
             <Tab.Screen 
                 name="NewBook" 
                 component={NewBookScreen} 
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({focused}) => {
+                        return <Ionicons 
+                                    name={'book'} 
+                                    size={25} 
+                                    color={
+                                        focused ? 'black' : 'gray'
+                                    } 
+                                />;
+                    }
+                }}
             />
         </Tab.Navigator>
     )
