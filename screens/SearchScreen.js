1799a54fdb71acc2043d4a33085e5794877c4561
Liam Sutton <liamsutton1997@outlook.com>
2022-02-10 21:16:11 +0000

    filter by genre, author

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, TextInput } from 'react-native';
 import { View, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker'
 import BookList from '../components/BookList';
 import { getAllBooks, getAllGenres, searchBooksByTitle } from '../database/db';
-import { NavigationContainer } from '@react-navigation/native';
-
-const SearchScreen = ({navigation}) => {
+const SearchScreen = ({ navigation }) => {
 
     const [isLoading, setIsLoading] = useState(true)
     const [masterBookList, setMasterBookList] = useState([])
     const [bookList, setBookList] = useState([])
     const [genreList, setGenreList] = useState([])
-    const [searchBy, setSearchBy] = useState(null)
+    const [filterBy, setFilterBy] = useState('title')
     useEffect(() => {
         if (navigation.isFocused()) {
             fetchData()
@@ -29,37 +29,81 @@ const SearchScreen = ({navigation}) => {
         setIsLoading(false)
     }
 
-    const searchByTitle = (text) => {
-        setSearchBy(text)
+    const filterBookList = (text) => {
+        let books = null
+        if (filterBy == 'title') {
+            books = filterByTitle(text)
+        }
+        if (filterBy == 'author') {
+            books = filterByAuthor(text)
+        }
+        if (filterBy == 'genre') {
+           books = filterByGenre(text)
+        }
+
+        setBookList(books)
+    }
+
+    const filterByTitle = (text) => {
+        text = text.trim()
         const books = masterBookList.filter((book) => {
             return book.title.toLowerCase().includes(text.toLowerCase())
         })
-        setBookList(books)
+       
+        return books
+    }
+
+    const filterByAuthor = (text) => {
+        const books = masterBookList.filter((book) => {
+            return book.author.toLowerCase().includes(text.toLowerCase())
+        })
+
+        return books
+    }
+
+    const filterByGenre = (text) => {
+        const books = masterBookList.filter((book) => {
+            return book.genre.toLowerCase().includes(text.toLowerCase())
+        })
+
+        return books
     }
     return (
         <SafeAreaView style={Styles.containerDark}>
-            <View>
-            <TextInput
-               style={Styles.textInputDark}
-               onChangeText={(text) => {
-                   searchByTitle(text)
-               }}
-               >
-
-               </TextInput>
+            <View style={Styles.searchContainer}>
+                <TextInput
+                    style={Styles.textInputDark}
+                    onChangeText={(text) => {
+                        filterBookList(text)
+                    }}
+                >
+                </TextInput>
+                <View style={Styles.genrePickerContainer}>
+                <Picker
+                    dropdownIconColor={'#FFFFFF'}
+                    style={Styles.genrePicker}
+                        selectedValue={filterBy}
+                        onValueChange={(itemValue, itemIndex) =>
+                            setFilterBy(itemValue)
+                        }>
+                        <Picker.Item label="Title" value="title" />
+                        <Picker.Item label="Author" value="author"/>
+                        <Picker.Item label="Genre" value="genre"/>
+                </Picker>
+                </View>
             </View>
-        {
-            isLoading ?
-            <Text style={Styles.textDark}>Loading books...</Text> :
-            <BookList books={bookList} genres={genreList} />
-        }
+            {
+                isLoading ?
+                    <Text style={Styles.textDark}>Loading books...</Text> :
+                    <BookList books={bookList} genres={genreList} />
+            }
         </SafeAreaView>
     )
 }
 const Styles = StyleSheet.create({
     containerDark: {
         flex: 1,
-        alignItems: 'flex-start',
+        flexDirection: 'column',
         backgroundColor: '#121212',
     },
 
@@ -69,13 +113,30 @@ const Styles = StyleSheet.create({
         color: 'white'
     },
     textInputDark: {
-        width: 300,
         height: 40,
         margin: 12,
         borderWidth: 1,
         padding: 10,
         color: 'white',
-        backgroundColor: '#181818'
+        backgroundColor: '#212121',
+    },
+
+    genrePickerContainer: {
+        backgroundColor: '#212121',
+        borderWidth: 1,
+        marginLeft: 12,
+        marginRight: 12,
+    },
+
+    genrePicker: {
+        color: 'white',
+        height: 40,
+        marginLeft: 12,
+        marginRight: 12,
+        borderWidth: 1,
+    },
+
+    genrePickerItem: {
     },
 })
 
